ea86474042d28147150352d145dd994ee3beacae
鈴木健人 <13bn075c@rikkyo.ac.jp>
2016-11-15 09:29:08 +0800

    14日のやるべきことを大方終了

---
+++ checkNote/checkNote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  checkNote
+//
+//  Created by 鈴木健人 on 2016/11/14.
+//  Copyright © 2016年 Kento Suzuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
