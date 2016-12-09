89d80b8282c5768d66a1f48e8d847c0745acc0c4
鈴木健人 <13bn075c@rikkyo.ac.jp>
2016-12-09 22:41:22 +0800

    デザインの背景としてimageviewを使った。

--- topView/topView/noteCheckViewController.swift
+++ topView/topView/noteCheckViewController.swift
@@ -65,28 +65,7 @@ class noteCheckViewController: UIViewController {
         
     }
     
-//    編集機能参考
-//    
-//    var df = DateFormatter()
-//    df.dateFormat = "yyyy/MM/dd hh:mm:ss +0000"
-//    df.timeZone = TimeZone.current
-//    //保存していた日付を文字列からDate型に変換
-//    var savedDateTime:NSDate = df.date(from: "\(myAp.myCount)") as! NSDate
-//    
-//    print(savedDateTime)
-//    
-//    
-//    //検索条件として指定
-//    let predicate = NSPredicate(format: "SELF.created_at = %@", savedDateTime )
-//    query.predicate = predicate
-//    
-//    
-//    do {
-//    let fetchResults = try viewContext.fetch(query)
-//    
-    
 //    ------------↓編集機能--------------------
-    
     @IBAction func editButton(_ sender: UIButton) {
         var titleEdit = myTitle.text
         var purposeEdit = myPurpose.text
@@ -102,38 +81,38 @@ class noteCheckViewController: UIViewController {
         let viewContext = appDelegate.persistentContainer.viewContext
         let request: NSFetchRequest<Note> = Note.fetchRequest()
         
-//        createとtitleで検索して取ってくる
+        //        createとtitleで検索して取ってくる
         
-//        〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜
-//        
-//        var request = NSFetchRequest(entityName: "Answers")
-//        request.returnsObjectsAsFaults = false;
-
-//        let resultPredicate1 = NSPredicate(format: "title = %i", titleEdit! and )
-//        
-//        let resultPredicate2 = NSPredicate(format: "created_at = %@", created_atEdit!)
-//        
-//        var compound = NSCompoundPredicate.andPredicate(withSubpredicates: [resultPredicate1, resultPredicate2])
-//        request.predicate = compound
+        //        〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜
+        //
+        //        var request = NSFetchRequest(entityName: "Answers")
+        //        request.returnsObjectsAsFaults = false;
         
-//        ＾＾＾＾＾＾＾＾＾＾＾＾＾＾＾＾
-//        let fetchRequest: NSFetchRequest<Staff> = Staff.fetchRequest()
+        //        let resultPredicate1 = NSPredicate(format: "title = %i", titleEdit! and )
+        //
+        //        let resultPredicate2 = NSPredicate(format: "created_at = %@", created_atEdit!)
+        //
+        //        var compound = NSCompoundPredicate.andPredicate(withSubpredicates: [resultPredicate1, resultPredicate2])
+        //        request.predicate = compound
+        
+        //        ＾＾＾＾＾＾＾＾＾＾＾＾＾＾＾＾
+        //        let fetchRequest: NSFetchRequest<Staff> = Staff.fetchRequest()
         print(noteCheck["created_atCheck"])
         
         let df = DateFormatter()
-//        df.dateFormat = "yyyy/MM/dd"
+        //        df.dateFormat = "yyyy/MM/dd"
         df.dateFormat = "yyyy/MM/dd hh:mm:ss"
         df.timeZone = TimeZone.current
         var created_atEditDate:NSDate = df.date(from: noteCheck["created_atCheck"] as! String ) as! NSDate
         
-//        var df = DateFormatter()
+        //        var df = DateFormatter()
         //    df.dateFormat = "yyyy/MM/dd hh:mm:ss +0000"
         //    df.timeZone = TimeZone.current
         //    //保存していた日付を文字列からDate型に変換
         //    var savedDateTime:NSDate = df.date(from: "\(myAp.myCount)") as! NSDate
         //
         //    print(savedDateTime)
-
+        
         
         
         
@@ -142,26 +121,26 @@ class noteCheckViewController: UIViewController {
             request.predicate = NSPredicate(format: "created_at = %@", created_atEditDate )
         }
         
-//        ======================================
+        //        ======================================
         
         //検索条件として指定
-//            let titkeserch = NSPredicate(format: "title = %@", titleEdit! )
-//            let timeserch = NSPredicate(format: "SELF.created_at = %@", savedDateTime )
-//            query.predicate = predicate
-
-    
-        
-//        ＾＾＾＾＾＾＾＾＾＾＾＾＾＾
-        
-//        〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜
+        //            let titkeserch = NSPredicate(format: "title = %@", titleEdit! )
+        //            let timeserch = NSPredicate(format: "SELF.created_at = %@", savedDateTime )
+        //            query.predicate = predicate
         
         
-         do {
+        
+        //        ＾＾＾＾＾＾＾＾＾＾＾＾＾＾
+        
+        //        〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜
+        
+        
+        do {
             let fetchResults = try viewContext.fetch(request)
             for result: AnyObject in fetchResults {
                 let record = result as! NSManagedObject
                 //                別のテキストフォルダーを変数で指定して、その値をDATEの中に書く
-//                record.setValue(Date(), forKey: "created_at")
+                //                record.setValue(Date(), forKey: "created_at")
                 
                 record.setValue(titleEdit, forKey: "title")
                 record.setValue(purposeEdit, forKey: "purpose")
@@ -173,15 +152,16 @@ class noteCheckViewController: UIViewController {
             try viewContext.save()
         } catch {
         }
+        
     }
     
-//    -------------↑変種機能------------------
-
+    
+// ----------編集機能↑------------------
 
 @IBAction func myButton(_ sender: UIButton) {
     //        トップページに戻るコードを書く
     navigationController?.popToViewController(navigationController!.viewControllers[0], animated: true)
-}
+    }
 
 
 override func didReceiveMemoryWarning() {
